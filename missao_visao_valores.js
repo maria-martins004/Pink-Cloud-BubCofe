ba4549a58771f6d8fee3b3deb2b63ab66072d94d
Maria Eduarda Martins <martins.maedu@gmail.com>
2025-07-11 12:02:09 -0300

    Responsividade do arquivo missao_visao_valores finalizado, e acréscimo de atalhos para as outras páginas

--- missao_visao_valores.js
+++ missao_visao_valores.js
@@ -17,7 +17,7 @@ const intervalo = setInterval(() => {
 
 // Faz o esboço rosa surgir com animação ao rolar a página
 document.addEventListener('DOMContentLoaded', function () {
-    const esboco = document.querySelector('.esboco_rosa');
+    const esboco = document.querySelector('.nossa_historia');
 
     function verificarEsboco() {
         const posicao = esboco.getBoundingClientRect();
@@ -29,4 +29,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', verificarEsboco);
     verificarEsboco(); // dispara se já estiver visível ao carregar
+});
+
+// Função de Responsividade para fazer parágrafo desaparecer e deixar apenas o span de todos os direitos reservados
+document.addEventListener('DOMContentLoaded', function () {
+    const p = document.querySelector('#paragrafo-desaparece');
+    const mediaQuery = window.matchMedia ('(max-width: 668px)');
+
+    function removerp(pparagrafo) {
+        if (pparagrafo.matches) {
+            p.style.display = "none";
+        } else {
+            p.style.display = "flex";
+        }
+    }
+    removerp(mediaQuery);
+    
+    mediaQuery.addEventListener('change', removerp);
 });
